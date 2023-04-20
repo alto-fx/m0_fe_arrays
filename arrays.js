d29531eabb29d4bdd2e3300136ae008d127091f3
Devin Altobello <dl.altobello@gmail.com>
2023-04-19 20:23:24 -0400

    Comment out explanations of methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var weekDays = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+weekDays.shift();
+console.log (weekDays);
+// Monday, the first element will be removed
 var num = [ 1, 2, 3, 4, 5,]
-var trueFalse = ["true", "true", "true", "false", "false"]
+num.push('3' , '4' , '5');
+console.log (num);
+// 3,4,5 will be added after 5
+var trueFalse = [true, true, true, false, true]
+trueFalse.unshift(false, false, false);
+console.log(trueFalse)
+// The new false booleans will be added at the end of the array
+
